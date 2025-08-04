c0aba97fb11db0c88c9976b080d5365b7c566723
miguelm-revel <miguel.melo@driverevel.com>
2025-08-04 16:46:21 +0200

    @cache ahora acepta argumentos posicionales y de clave

--- decotools/cache.py
+++ decotools/cache.py
@@ -1,7 +1,12 @@
 def cache(func):
     func_cache = {}
-    def wrapper(*args):
-        key = frozenset(args)
+    def wrapper(*args, **kwargs):
+        args_key = frozenset(args)
+        kwargs_key = []
+        for k, v in kwargs.items():
+            kwargs_key.append((k, v))
+        kwargs_key = frozenset(kwargs_key)
+        key = (args_key, kwargs_key)
         if key in func_cache:
             return func_cache[key]
         else:
